3789b6fbc18d2faadbfd61fa4b241b99747aa34e
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-05 01:45:55 +0900

    [Update]setIntervalを使った無理やりの更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 import axios from 'axios';
@@ -12,6 +12,7 @@ interface Message {
 
 function App() {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [inputText, setInputText] = useState('');
   useEffect(() => {
     connectTest();
   }, []);
@@ -24,9 +25,32 @@ function App() {
       })
       .catch((e) => console.log(e));
   };
+
+  const changeInputText = (e: any) => {
+    setInputText(e.target.value);
+  };
+
+  const clickSendMessage = async () => {
+    await axios
+      .post('http://localhost:3000/test/tests', { message: inputText })
+      .then((res) => {
+        setInputText('');
+        setMessages([...messages, res.data.message]);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  setInterval(() => {
+    connectTest();
+  }, 1000);
+
   return (
     <div className="App">
+      <h1>ChatRoom</h1>
       <ul>{messages !== [] ? messages.map((message) => <li key={message.id}>{message.body}</li>) : null}</ul>
+      <label htmlFor="">Say Something</label>
+      <input type="text" value={inputText} onChange={changeInputText} />
+      <button onClick={clickSendMessage}>送信</button>
     </div>
   );
 }
